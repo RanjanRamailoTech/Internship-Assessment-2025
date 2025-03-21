b4657e0c076c8b6e590765a796ae0d76a7201d04
ranjanlamsal <lamsalranjan19@gmail.com>
2025-03-22 02:51:05 +0545

    deleted a test file

--- Day4-Django-advanced/django-starter/test_env.py
+++
@@ -1,5 +0,0 @@
-import os
-from decouple import config
-
-print("os.environ.get('CELERY_BROKER_URL'):", os.environ.get('CELERY_BROKER_URL'))
-print("config('CELERY_BROKER_URL'):", config('CELERY_BROKER_URL', default='Not found'))
